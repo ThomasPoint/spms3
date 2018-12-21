262b944c94e112cbf37ec811bb05f36acfc72449
ThomasPoint <pointthomas84700@gmail.com>
2018-12-21 06:20:11 +0200

    Updates

---
+++ precache-manifest.9e0e633c802dfc77c33bca5c4ec971f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spms3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e7d0bd8c46a7af404ad",
+    "url": "/spms3/static/js/runtime~main.4e7d0bd8.js"
+  },
+  {
+    "revision": "82f8548dac2af4f3ad87",
+    "url": "/spms3/static/js/main.82f8548d.chunk.js"
+  },
+  {
+    "revision": "a919c1db010828fdc862",
+    "url": "/spms3/static/js/1.a919c1db.chunk.js"
+  },
+  {
+    "revision": "82f8548dac2af4f3ad87",
+    "url": "/spms3/static/css/main.442f3f42.chunk.css"
+  },
+  {
+    "revision": "a919c1db010828fdc862",
+    "url": "/spms3/static/css/1.9c30d27c.chunk.css"
+  },
+  {
+    "revision": "da66b12b87da8659ea511042ced0a9ec",
+    "url": "/spms3/index.html"
+  }
+];
